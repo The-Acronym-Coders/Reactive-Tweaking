6739f1b1db6ead78732f684c3cbc03c7281d7aaf
Skylar Sommers <ssommers@criticalmix.com>
2016-10-24 23:33:51 -0400

    Start of Mod file

---
+++ src/main/java/com/teamacronymcoders/reactivetweaker/ReactiveTweaker.java
@@ -0,0 +1,9 @@
+package java.com.teamacronymcoders.reactivetweaker;
+
+
+public class ReactiveTweaker {
+    public final String MODID = "reactivetweaker";
+    public final String NAME = "Reactive Tweaker";
+    public final String VERSION = "@VERSION@";
+    public final String DEPENDS = "required-after:MineTweaker3;required-after:bigreactors;";
+}
