8e7bff3d3faaf601b9ae09a31456e0db76757486
Jared <jaredlll08@gmail.com>
2016-10-27 09:42:29 +0200

    Moved to Optionals

--- src/main/java/com/teamacronymcoders/reactivetweaker/RTReactants.java
+++ src/main/java/com/teamacronymcoders/reactivetweaker/RTReactants.java
@@ -1,51 +1,41 @@
 package com.teamacronymcoders.reactivetweaker;
 
 import erogenousbeef.bigreactors.api.registry.Reactants;
+import minetweaker.api.item.IIngredient;
+import minetweaker.api.item.IngredientItem;
+import minetweaker.api.liquid.ILiquidStack;
+import minetweaker.api.oredict.IngredientOreDict;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
+import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 @ZenClass("mod.reactivetweaker.Reactants")
 public class RTReactants {
+
     @ZenMethod
-    public static void registerReactants(String name, boolean fuel) {
-        Reactants.registerReactant(name, fuel);
+    public static void registerReactants(String name, boolean fuel, @Optional int color) {
+        Reactants.registerReactant(name, (fuel ? 0 : 1), color);
     }
 
     @ZenMethod
-    public static void registerReactant(String name, int type, int color) {
-        Reactants.registerReactant(name, type, color);
+    public static void registerSolid(IIngredient ing, String reactantName, @Optional int reactantQty) {
+        if(reactantQty == 0) {
+            reactantQty = 1000;
+        }
+        if(ing instanceof IngredientOreDict) {
+            IngredientOreDict ore = (IngredientOreDict) ing;
+            Reactants.registerSolid((String) ore.getInternal(), reactantName, reactantQty);
+        } else if(ing instanceof IngredientItem) {
+            IngredientItem item = (IngredientItem) ing;
+            Reactants.registerSolid((ItemStack) item.getInternal(), reactantName, reactantQty);
+        }
     }
 
     @ZenMethod
-    public static void registerSolid(ItemStack itemStack, String reactantName) {
-        Reactants.registerSolid(itemStack, reactantName);
+    public static void registerFluid(ILiquidStack stack, String reactantName) {
+        Reactants.registerFluid((FluidStack) stack.getDefinition().asStack(stack.getAmount()).getInternal(), reactantName);
     }
 
-    @ZenMethod
-    public static void registerSolid(ItemStack itemStack, String reactantName, int reactantQty) {
-        Reactants.registerSolid(itemStack, reactantName, reactantQty);
-    }
-
-    @ZenMethod
-    public static void registerSolid(String oreDictName, String reactantName, int reactantAmount) {
-        Reactants.registerSolid(oreDictName, reactantName, reactantAmount);
-    }
-
-    @ZenMethod
-    public static void registerSolid(String oreDictName, String reactantName) {
-        Reactants.registerSolid(oreDictName, reactantName);
-    }
-
-    @ZenMethod
-    public static void registerFluid(FluidStack fluidStack, String reactantName) {
-        Reactants.registerFluid(fluidStack, reactantName);
-    }
-
-    @ZenMethod
-    public static void registerFluid(Fluid fluid, String reactantName) {
-        Reactants.registerFluid(fluid, reactantName);
-    }
 }
--- src/main/java/com/teamacronymcoders/reactivetweaker/RTReactorConversions.java
+++ src/main/java/com/teamacronymcoders/reactivetweaker/RTReactorConversions.java
@@ -1,18 +1,21 @@
 package com.teamacronymcoders.reactivetweaker;
 
 import erogenousbeef.bigreactors.api.registry.ReactorConversions;
+import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 @ZenClass("mods.reactivetweaker.ReactorConversions")
 public class RTReactorConversions {
     @ZenMethod
-    public static void register(String sourceName, String productName) {
-        ReactorConversions.register(sourceName, productName);
-    }
-
-    @ZenMethod
-    public static void register(String sourceName, String productName, float reactivity, float fissionRate) {
+    public static void register(String sourceName, String productName, @Optional float reactivity, @Optional float fissionRate) {
+        if(reactivity == 0) {
+            reactivity = 1.05F;
+        }
+        if(fissionRate == 0) {
+            fissionRate = 0.01F;
+        }
         ReactorConversions.register(sourceName, productName, reactivity, fissionRate);
     }
+
 }
